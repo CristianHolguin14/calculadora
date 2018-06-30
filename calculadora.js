c9c5de6cc0c762f45df50d6f97d0044b9f3b3809
cristian <cristianholguin00@gmail.com>
2018-06-30 12:13:36 -0500

    calculadora

---
+++ calculadora.js
@@ -0,0 +1,5 @@
+var radio, altura
+radio=parseInt(prompt("ingresar el radio:") );
+altura=parseInt(prompt("ingresar la altura:") );
+alert("el volumen del cilindro es: " + (Math.PI * radio * radio * altura) );
+
